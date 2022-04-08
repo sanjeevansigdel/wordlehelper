275a5a17eb36dbe7e279c6a8b08a805c1de81094
Sanjeevan <sanjeevansigdel@outlook.com>
2022-04-07 22:18:06 -0500

    Updates

---
+++ precache-manifest.cb94cf261d8daf1d9b7880a59ba824ad.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "13772acef176213d6abc",
+    "url": "/wordlehelper/static/css/main.c86bc125.chunk.css"
+  },
+  {
+    "revision": "13772acef176213d6abc",
+    "url": "/wordlehelper/static/js/main.13772ace.chunk.js"
+  },
+  {
+    "revision": "9eb8d3dfd3c7c6f43da7",
+    "url": "/wordlehelper/static/js/1.9eb8d3df.chunk.js"
+  },
+  {
+    "revision": "dc90cb8ef59ab2e2f527",
+    "url": "/wordlehelper/static/js/2.dc90cb8e.chunk.js"
+  },
+  {
+    "revision": "80a901c84fad85d34920",
+    "url": "/wordlehelper/static/js/runtime~main.80a901c8.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/wordlehelper/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "9ece5b48963bbc96309220952cda38aa",
+    "url": "/wordlehelper/static/media/Roboto-Bold.9ece5b48.ttf"
+  },
+  {
+    "revision": "f36638c2135b71e5a623dca52b611173",
+    "url": "/wordlehelper/static/media/Roboto-Regular.f36638c2.ttf"
+  },
+  {
+    "revision": "301fe70f8f0f41c236317504ec05f820",
+    "url": "/wordlehelper/static/media/Roboto-Black.301fe70f.ttf"
+  },
+  {
+    "revision": "32a99b2fa82125b73e8a9c3f2d485ee5",
+    "url": "/wordlehelper/index.html"
+  }
+];
